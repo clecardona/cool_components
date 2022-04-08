37650f6fb030059950fbdf5aa479b40fcd94be6b
clecardona <clecardona@gmail.com>
2022-04-08 09:03:48 +0200

    hovering style

--- src/App.tsx
+++ src/App.tsx
@@ -6,46 +6,21 @@ import Sayan from "components/Battles/Sayan/Sayan";
 import Apple from "components/Battles/Apple/Apple";
 import Pirate from "components/Battles/Pirate/Pirate";
 import IceCream from "components/Battles/IceCream/IceCream";
-import { useEffect, useState } from "react";
 import Sceptre from "components/Battles/Sceptre/Sceptre";
+import Carousel from "components/Carousel";
 
 function App() {
-  const [scrollY, setscrollY] = useState(0);
-  const [canScrollRight, setcanScrollRight] = useState(true);
-  const [canScrollLeft, setcanScrollLeft] = useState(false);
+  const battles = [
+    <Sayan />,
+    <Apple />,
+    <Pirate />,
+    <IceCream />,
+    <Sceptre />,
+    <Pirate />,
+    <IceCream />,
+    <Sceptre />,
+  ];
   const BOX_WIDTH = 800;
-
-  useEffect(() => {
-    const fullWidth = document.getElementById("box")?.offsetWidth;
-    if (scrollY === 0) {
-      setcanScrollLeft(false);
-      setcanScrollRight(true);
-    }
-    if (scrollY >= BOX_WIDTH) {
-      setcanScrollLeft(true);
-      setcanScrollRight(true);
-    }
-    if (fullWidth) {
-      if (scrollY + BOX_WIDTH >= fullWidth) {
-        setcanScrollLeft(true);
-        setcanScrollRight(false);
-      }
-    }
-  }, [scrollY]);
-
-  function scrollRight() {
-    const fullWidth = document.getElementById("box")?.offsetWidth;
-    if (fullWidth) {
-      if (scrollY + BOX_WIDTH >= fullWidth) return;
-      else return setscrollY(scrollY + BOX_WIDTH);
-    }
-  }
-
-  function scrollLeft() {
-    if (scrollY <= 0) return;
-    else return setscrollY(scrollY - BOX_WIDTH);
-  }
-
   return (
     <div className="App">
       <header>
@@ -55,37 +30,11 @@ function App() {
       </header>
       <main>
         <MagicMenu />
-
         <div className="content-battle">
           <h2 className="float">
             CSS <b>Battles</b>
           </h2>
-          {/* <div className="battle-grid"> */}
-          <div className="carousel" style={{ width: BOX_WIDTH }}>
-            {canScrollRight && (
-              <button id="br" onClick={() => scrollRight()}>
-                <p>{">"}</p>
-              </button>
-            )}
-            {canScrollLeft && (
-              <button id="bl" onClick={() => scrollLeft()}>
-                <p>{"<"}</p>
-              </button>
-            )}
-
-            <div id="box" style={{ left: -scrollY }}>
-              <Sayan />
-              <Apple />
-              <Pirate />
-              <IceCream />
-              <Sceptre />
-              <IceCream />
-              <Sayan />
-              <Pirate />
-              <Sayan />
-              <IceCream />
-            </div>
-          </div>
+          <Carousel Battles={battles} width={BOX_WIDTH} />
         </div>
       </main>
     </div>
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+  Battles: JSX.Element[];
+  width: number;
+};
+
+const Carousel = ({ Battles, width }: Props) => {
+  const [canScrollRight, setcanScrollRight] = useState(true);
+  const [canScrollLeft, setcanScrollLeft] = useState(false);
+  const [scrollY, setscrollY] = useState(0);
+  const [current, setcurrent] = useState(-99);
+
+  function scrollRight() {
+    const fullWidth = document.getElementById("box")?.offsetWidth;
+    if (fullWidth) {
+      if (scrollY + width >= fullWidth) return;
+      else return setscrollY(scrollY + width);
+    }
+  }
+
+  function scrollLeft() {
+    if (scrollY <= 0) return;
+    else return setscrollY(scrollY - width);
+  }
+
+  useEffect(() => {
+    const fullWidth = document.getElementById("box")?.offsetWidth;
+    if (scrollY === 0) {
+      setcanScrollLeft(false);
+      setcanScrollRight(true);
+    }
+    if (scrollY >= width) {
+      setcanScrollLeft(true);
+      setcanScrollRight(true);
+    }
+    if (fullWidth) {
+      if (scrollY + width >= fullWidth) {
+        setcanScrollLeft(true);
+        setcanScrollRight(false);
+      }
+    }
+  }, [scrollY, width]);
+
+  const ListOfBattles = Battles.map((Comp, index) => {
+    return (
+      <a
+        onMouseOver={() => setcurrent(index)}
+        onMouseOut={() => setcurrent(-99)}
+        href="https://cssbattle.dev/"
+        target="#"
+        className={
+          index === current
+            ? "current"
+            : index === current - 1 || index === current + 1
+            ? "adjacent"
+            : "standard"
+        }
+      >
+        {Comp}
+      </a>
+    );
+  });
+
+  return (
+    <div className="carousel" style={{ width: width }}>
+      {canScrollRight && (
+        <button id="br" onClick={() => scrollRight()}>
+          <p>{">"}</p>
+        </button>
+      )}
+      {canScrollLeft && (
+        <button id="bl" onClick={() => scrollLeft()}>
+          <p>{"<"}</p>
+        </button>
+      )}
+      <div id="box" style={{ left: -scrollY }}>
+        {ListOfBattles}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
